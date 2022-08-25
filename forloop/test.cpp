185a885e729373e28bc7adc33cc26939bb5e016f
mishael254 <mishaelmishantoz@gmail.com>
2022-08-25 17:02:10 +0300

    create a new empty project

---
+++ forloop/test.cpp
@@ -0,0 +1,12 @@
+#include "test.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 100; ++i) {
+		cout << i << '\t' << pow(i, 2) << endl;
+	}
+
+}
